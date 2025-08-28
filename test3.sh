47bb29ac938963e47bc78bd74dda78c16e520972
Kas-sim <amkassim444@gmail.com>
2025-08-28 21:42:23 +0500

    Scripting continued

--- test3.sh
+++ test3.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo run from anywhere?
+echo What is your first name?
+read FIRST_NAME
+echo What is your last name?
+read LAST_NAME
+
+echo AssalamuAlaikum $FIRST_NAME bin $LAST_NAME
+
+Hello World of Scripting
