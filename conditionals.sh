62fdaedd7a517ed8b796e1c80ed2b8b655670271
Kas-sim <amkassim444@gmail.com>
2025-09-02 23:17:27 +0500

    First conditionals - aah Struggle

--- conditionals.sh
+++ conditionals.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-okay of you
-    lol
-    that's funny
-    in the middle of
-    a night
-- it stoo dark
+echo "Guess what number I have typed"
+read num1
+
+num0=69
+
+if [[ $num0 > $num1 ]]; then
+    echo "Raise your Standards"
+elif [[ $num0 < $num1 ]]; then
+    echo "Lower your Standards"
+else
+     echo " You gto a taste ;) "
+fi
