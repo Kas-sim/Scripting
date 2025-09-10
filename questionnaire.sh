19982808f88253d065ef6bec1f1a999468dcaeb1
Kas-sim <amkassim444@gmail.com>
2025-09-11 00:36:19 +0500

    another file checker and readable checker

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "Questionnaire"
-echo "lol - You are working!"
+echo "Check if file exist"
+read file_name
+
+if [[ -f "$file_name" && -r "$file_name" ]]; then
+	echo "File exists and is readable"
+else 
+	echo "File does not exist"
+fi
+
