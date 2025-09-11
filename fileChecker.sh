c67e7ca0dbe64fd2c7978f83ee4e674fd11699bb
Kas-sim <amkassim444@gmail.com>
2025-09-11 16:10:47 +0500

    Project 2 - File Checker (modified)

--- fileChecker.sh
+++ fileChecker.sh
@@ -2,12 +2,14 @@
 
 
 
-read -p "Enter file you are looking for (in home directory): " file
+read -p "Enter file you are looking for (in current directory - do not write .sh): " file
 
-if [[ -f $file ]]; then
-    echo "$file exists and is a file"
-elif [[ -d $file ]]; then
-    echo "$file exists and is a directory"
+if [[ -f "$file.sh" && -r "$file.sh" ]]; then
+	echo "$file exists and is readable"
+elif [[ -f "$file.sh" ]]; then
+	echo "$file exists!"
+elif [[ -d "$file.sh" ]]; then
+	echo "$file exists and is a directory"
 else
-    echo "$file doesn't exist in home directory"
+	echo "$file doesn't exist in home directory"
 fi
